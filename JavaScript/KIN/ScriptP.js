d875ef926b37395de1b9b7d27c5aa0807ae0d3e7
WeiBnesN <lonavere@gmail.com>
2025-02-20 20:01:08 +0500

    почти Кинопоиск))) ++(+-)-+

--- JavaScript/KIN/ScriptP.js
+++ JavaScript/KIN/ScriptP.js
@@ -1,6 +1,8 @@
 let movie__title = document.getElementsByClassName('movie__title')
 let movie__cover = document.getElementsByClassName('movie__cover')
 let movieEe = []
+let countriessGen = []
+let genres = []
 
 const api_key = 'f85183cb-777b-4fe8-a1c7-e368548fac6f'
 let mobie = null
@@ -35,14 +37,61 @@ function showFilm() {
     let mins = document.getElementById('mins')
     let rate_Imdb = document.getElementById('rate_Imdb')
     let rate_Kin = document.getElementById('rate_Kin')
+    let pButton = document.getElementById('pButton')
+    let buttton = document.getElementById('sub')
+    for (let co of mobie.countries) {
+        country.innerHTML += co.country + ', '
+    }
 
+    for (let gen of mobie.genres) {
+        g.innerHTML += gen.genre + ', '
+    }
+
+
+    // countriessGen.push({
+    //     country: mobie.countries,
+    //     genre: mobie.genres
+    // })
+    //
+    // for (let co of countriessGen) {
+    //     country.innerHTML = co.country.country
+    //     g.innerHTML = co.genre.genre
+    // }
         cover.src = mobie.posterUrl
         title.innerHTML = mobie.nameRu
         content.innerHTML = mobie.description
     year.innerHTML = mobie.year
-    country.innerHTML = mobie.countries
-    g.innerHTML = mobie.genres
-    mins.innerHTML = mobie.filmLength
-    rate_Imdb.innerHTML = mobie.ratingImdb
+
+
+    mins.innerHTML = mobie.filmLength || '-'
+    rate_Imdb.innerHTML = mobie.ratingImdb || '-'
     rate_Kin.innerHTML = mobie.ratingKinopoisk
+    pButton.href = mobie.webUrl
+
+
+    // if (mobie.ratingImdb == null){
+    //     rate_Imdb.innerHTML = '-'}
+
+    buttton.addEventListener('click', sendComment)
+    function sendComment() {
+        let author = document.getElementById('author')
+        let comment = document.getElementById('text')
+        let comlist = document.getElementById('comlist')
+
+
+        let CommentCard = document.createElement('div')
+        let CommentTitle = document.createElement('h3')
+        let CommentAuthor = document.createElement('h2')
+
+        CommentTitle.classList.add('comment-form__text')
+        CommentAuthor.classList.add('comment-form__author')
+        CommentCard.classList.add('comments-list__item')
+
+        CommentTitle.innerHTML = comment.value
+        CommentAuthor.innerHTML = author.value
+
+        CommentTitle.append(CommentCard)
+        CommentAuthor.append(CommentCard)
+        CommentCard.append(comlist)
+    }
 }
